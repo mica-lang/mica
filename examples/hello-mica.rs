c4301e2e448fe01f0344cb1cf88b35cf2a4a9f4c
lqdev <liquidev@tutanota.com>
2022-03-07 17:18:42 +0100

    example code

---
+++ examples/hello-mica.rs
@@ -0,0 +1,13 @@
+//! An example of how to evaluate code using Mica.
+
+use mica::Engine;
+
+fn main() {
+   let engine = Engine::new();
+   mica::std::load(&engine).unwrap();
+
+   engine.set("x", 123_i32).unwrap();
+   engine.set("y", 456_i32).unwrap();
+   let result: i32 = engine.start("<main>", "x * 3 + y").unwrap().trampoline().unwrap();
+   println!("{result}");
+}
--- mica-hl/src/fiber.rs
+++ mica-hl/src/fiber.rs
@@ -4,7 +4,7 @@ use std::rc::Rc;
 use mica_language::value::Value;
 use mica_language::vm;
 
-use crate::{Error, RuntimeEnvironment};
+use crate::{Error, RuntimeEnvironment, TryFromValue};
 
 /// A fiber represents an independent, pausable thread of code execution.
 ///
@@ -20,14 +20,32 @@ pub struct Fiber {
 
 impl Fiber {
    /// Resumes execution of a fiber. If execution is done already, returns `None`.
-   pub fn resume(&mut self) -> Result<Option<Value>, Error> {
+   pub fn resume<T>(&mut self) -> Result<Option<T>, Error>
+   where
+      T: TryFromValue,
+   {
       if self.inner.halted() {
          Ok(None)
       } else {
          let mut runtime_env = self.runtime_env.try_borrow_mut().map_err(|_| Error::EngineInUse)?;
          let (env, globals) = runtime_env.split();
          let result = self.inner.interpret(env, globals)?;
-         Ok(Some(result))
+         Ok(Some(T::try_from_value(result)?))
       }
+   }
+
+   /// Resumes execution of a fiber until it's done evaluating all code. The last result is
+   /// returned and results from intermediate yields are discarded.
+   ///
+   /// This consumes the fiber, as calling one that's finished is not useful.
+   pub fn trampoline<T>(mut self) -> Result<T, Error>
+   where
+      T: TryFromValue,
+   {
+      let mut result = Value::Nil;
+      while let Some(v) = self.resume()? {
+         result = v;
+      }
+      T::try_from_value(result)
    }
 }

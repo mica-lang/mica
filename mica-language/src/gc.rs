a1bebbb6142852a1a19391986e8aaa989be90727
lqdev <liquidev@tutanota.com>
2022-06-07 20:00:57 +0200

    fix a couple cases of UB found by miri

--- mica-language/src/gc.rs
+++ mica-language/src/gc.rs
@@ -107,8 +107,9 @@ impl Memory {
             let memory = memories[i];
             let mem = memory.get_mem();
             if !mem.reachable.get() {
+               let data_size = mem.data_size;
                GcMem::release(memory);
-               *allocated_bytes -= mem.data_size;
+               *allocated_bytes -= data_size;
                #[cfg(feature = "trace-gc")]
                {
                   println!(
@@ -291,6 +292,9 @@ pub(crate) struct GcMem<T> {
    finalizer: unsafe fn(*mut u8),
    /// The size of the allocated data.
    data_size: usize,
+   /// The layout that was used for allocating this `GcMem<T>`; this is needed to deallocate
+   /// without triggering undefined behavior.
+   layout: Layout,
    /// The data.
    data: T,
 }
@@ -314,6 +318,7 @@ impl<T> GcMem<T> {
 
    /// Allocates a `GcMem<T>`.
    fn allocate(data: T, finalizer: unsafe fn(*mut u8)) -> GcRaw<T> {
+      let layout = Self::layout();
       let mem = Self {
          // NOTE: `reachable` is initially set to `false` because reachability is only determined
          // during the marking phase.
@@ -321,7 +326,9 @@ impl<T> GcMem<T> {
          managed_by_gc: Cell::new(true),
          rc: Cell::new(0),
          finalizer,
+         layout,
          data_size: std::mem::size_of::<T>(),
+         layout,
          data,
       };
       let layout = Self::layout();
@@ -351,13 +358,14 @@ impl<T> GcMem<T> {
       {
          println!("gcmem | deallocating {:p}", mem);
       }
+      let layout;
       {
          let mem = &mut *mem;
          (mem.finalizer)(&mut mem.data as *mut T as *mut u8);
-         // ManuallyDrop::drop(&mut mem.data);
+         layout = mem.layout;
       }
       // Ugh, that cast from *const to *mut hurts.
-      std::alloc::dealloc(mem as *mut u8, Self::layout())
+      std::alloc::dealloc(mem as *mut u8, layout)
    }
 
    /// Deallocates the given memory or marks it as unmanaged if there are foreign references to it.

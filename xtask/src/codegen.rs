310b1537cbce9e3fe8727752785eb458366e5065
lqdev <liquidev@tutanota.com>
2022-10-25 23:40:17 +0200

    rustfmt codegen'd files

--- xtask/src/codegen.rs
+++ xtask/src/codegen.rs
@@ -1,11 +1,19 @@
 mod function_variants;
 
-use tracing::info_span;
+use std::process::Command;
+
+use tracing::{info, info_span};
 
 pub fn run() -> anyhow::Result<()> {
     let _span = info_span!("codegen").entered();
 
-    function_variants::generate()?;
+    let generated_files = [function_variants::generate()?];
+
+    for path in &generated_files {
+        let _span = info_span!("rustfmt", ?path).entered();
+        let output = Command::new("rustfmt").arg(path).output()?;
+        info!(code = ?output.status, "rustfmt done");
+    }
 
     Ok(())
 }
